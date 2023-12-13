569a4403ab7c84cf20748076a272bc84f33aad11
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2023-12-13 20:35:49 +0530

    new

--- testmvc/app/views/addEmployee.php
+++ testmvc/app/views/addEmployee.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/assets/css/addEmployee.css">
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/addEmployee.css">
     <title>Admin Employee</title>
 </head>
 <div class="side-menu">
