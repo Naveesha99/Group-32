2b26724e848547a4f758d60162ac8794f92417c9
Nethmi Hasinthara <www.nethmihasi2001@gmail.com>
2024-01-04 10:01:45 +0700

    new

---
+++ testmvc/app/views/reservaSideBar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/reservaSideBar.css">
+
+
+
+    <title>Document</title>
+</head>
+
+
+<div class="side-menu">
+    <div class="brand-name">
+        <img src="<?= ROOT ?>/assets/images/Logo.png" id="logo">
+
+    </div>
+    <ul>
+        <a href="reserva_dashboard.html">
+            <li><img src="<?= ROOT ?>/assets/images/reservaDashboard.png" alt="">&nbsp; <span>Dashboard</span> </li>
+        </a>
+        <a href="reservaHall">
+            <li><img src="<?= ROOT ?>/assets/images/reservaHall.png" alt="">&nbsp; <span>Hall Request</span> </li>
+        </a>
+        <a href="reservaSentRequests.html">
+            <li><img src="<?= ROOT ?>/assets/images/reservaRequest.png" alt="">&nbsp; <span>Sent Requests</span> </li>
+        </a>
+        <a href="resrvaNoti.html">
+            <li><img src="<?= ROOT ?>/assets/images/reservaNotification.png" alt="">&nbsp; <span>Notifications</span> </li>
+        </a>
+        <a href="reservaSettings.html">
+            <li><img src="<?= ROOT ?>/assets/images/reservaSettings.png" alt="">&nbsp; <span>Settings</span> </li>
+        </a>
+    </ul>
+</div>
+
+</html>
