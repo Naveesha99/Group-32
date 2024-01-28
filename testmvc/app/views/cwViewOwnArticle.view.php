671045439398373fc1c25cf408dcf2acb438278f
Nethmi Hasinthara <www.nethmihasi2001@gmail.com>
2024-01-29 06:42:32 +0700

    own article

--- testmvc/app/views/cwViewOwnArticle.view.php
+++ testmvc/app/views/cwViewOwnArticle.view.php
@@ -10,9 +10,25 @@
 <?php require_once 'cwNaviBar.php' ?>
 
 <body>
-
-
     <div class="container">
+        <div class="card">
+            <img src="<?=ROOT?>/assets/images/home/i3.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <h1 class="card-title">EDIPAS RAJA</h1>
+                <p class="card-sub-title">Category : Tragedy</p>
+                <p class="card-info">Oedipus Rex by Sophocles is a Greek tragedy about a man who unknowingly fulfills a prophecy by killing his father and marrying his mother. It explores themes of fate, free will, and the consequences of unchecked pride.
+                The story of Oedipus is the subject of Sophocles' tragedy Oedipus Rex, which is followed in the narrative sequence by Oedipus at Colonus and then Antigone. Together, these plays make up Sophocles' three Theban plays.
+                 Oedipus represents two enduring themes of Greek myth and drama: the flawed nature of humanity and an individual's role in the course of destiny in a harsh universe.
+                </p>
+
+                <button class="card-btn" onclick="edit()" style="background-color: #00FF1A;">Edit</button>
+                <button class="card-btn" onclick="" style="background-color: #FF0000;">Delete</button>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- <div class="container">
         <h1 id="articleName" span>MANAME</h1>
         <div class="image">
             <img id="articleImage" src="<?=ROOT?>/assets/images/kuweni.jpeg" alt="Article Image">
@@ -36,7 +52,7 @@
 
 
         </div>
-    </div>
+    </div> -->
 
 </body>
 
