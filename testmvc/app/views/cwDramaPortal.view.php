6827b5ed047a600ba7ff8c0b8f72b0540d3f3b74
Nethmi Hasinthara <www.nethmihasi2001@gmail.com>
2024-01-28 23:47:16 +0700

    drama portal new

--- testmvc/app/views/cwDramaPortal.view.php
+++ testmvc/app/views/cwDramaPortal.view.php
@@ -4,126 +4,112 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Drama Portal</title>
     <link rel="stylesheet" href="<?=ROOT?>/assets/css/cwDramaPortal.css">
-
+    <title>Drama Portal</title>
 </head>
-
-
+<?php require_once 'cwNaviBar.php' ?>
 <body>
-
-    <div class="button-group">
-        <a href="<?=ROOT?>/cwAddArticle">
-            <button type="button" onclick="addNew()">ADD NEW</button>
-        </a>
-    </div>
-
-    <section class="container">
-
-        <!-- <p class="paragraph">Categories </p>
-        <div class="category">
-        
-            <select id="category" name="Category">
-                <option class="box" value="All">All</option>
-                <option class="box" value="DramaHistory">Drama History</option>
-                <option class="box" value="StoryLine">Story Line</option>
-                <option class="box" value="Directors">Directors</option>
-                <option class="box" value="Actors">Actors</option>
-                <option class="box" value="Upcoming">Upcoming</option>
-                <option class="box" value="DramaMusic">Drama Music</option>
-                </select>
-        </div> -->
-       
-        
+<div class="container">
         <div class="card">
-            <div class="card-image image-1 " >
-                <img src="<?=ROOT?>/assets/images/kuweni.jpeg" alt="kuweni" >
+            <div class="imgBX">
+                <img src="<?=ROOT ?>/assets/images/home/i3.jpg" alt="image" >
             </div>
-            <h2>Maname</h2>
-            <p>A drama by the celebrated dramatist, 
-                Prof Ediriweera Sarachchandra, that utilises the traditions of nadagama 
-                (a type of rural folk theatre in Sri Lanka). 
-            </p>
-            <a href="cwViewOwnArticle">READ MORE</a>
+
+            <div class="Content">
+            <h2>EDIPAS RAJA</h2>
+                <p>Category:Tragedy</p>
+                <p>Oedipus Rex by Sophocles is a Greek tragedy about a man who unknowingly fulfills a prophecy by killing his father and marrying his mother. It explores themes of fate, free will, and the consequences of unchecked pride.</p>
+                <a href="#">READ MORE</a>
+
+            </div>
         </div>
 
         <div class="card">
-            <div class="card-image image-2" ></div>
-            <h2>Maname</h2>
-            <p>A drama by the celebrated dramatist, 
-                Prof Ediriweera Sarachchandra, that utilises the traditions of nadagama 
-                (a type of rural folk theatre in Sri Lanka). 
-            </p>
-            <a href="cwViewOwnArticle">READ MORE</a>
-            
+            <div class="imgBX">
+                <img src="<?=ROOT ?>/assets/images/home/i10.jpeg" alt="" >
+            </div>
+            <div class="Content">
+            <h2>RAWANA</h2>
+                <p>Category:Tragedy</p>
+                <p>Lanka flourished under his rule and Ravana had proceeded on a series of campaigns conquering humans, celestials and demons. It is said that Ravana ruled Lanka for several hundred years prior to the times of Ramayana, when he was killed by prince Rama for kidnapping his wife Sita.</p>
+                <a href="#">READ MORE</a>
+            </div>
         </div>
 
         <div class="card">
-            <div class="card-image image-3" ></div>
-            <h2>Maname</h2>
-            <p>A drama by the celebrated dramatist, 
-                Prof Ediriweera Sarachchandra, that utilises the traditions of nadagama 
-                (a type of rural folk theatre in Sri Lanka). 
-            </p>
-            <a href="cwViewOwnArticle">READ MORE</a>
-
-            
+            <div class="imgBX">
+                <img src="<?=ROOT ?>/assets/images/home/i7.jpg" alt="">
+            </div>
+            <div class="Content">
+            <h2>ALADIN SAHA PUDUMA PAHANA</h2>
+                <p>Category:Tragedy</p>
+                <p>Set in the fictional Arabian city of Agrabah, the story follows the familiar tale of a poor young man who is granted three wishes by a genie in a lamp, which he uses to woo a princess and to thwart the sultan's evil Grand Vizier.</p>
+                <a href="#">READ MORE</a>
+            </div>
         </div>
 
         <div class="card">
-            <div class="card-image image-4" ></div>
-            <h2>Maname</h2>
-            <p>A drama by the celebrated dramatist, 
+            <div class="imgBX">
+                <img src="<?=ROOT ?>/assets/images/article.png" alt="">
+            </div>
+            <div class="Content">
+            <h2>MANAME</h2>
+                <p>Category:Tragedy</p>
+                <p>A drama by the celebrated dramatist, 
                 Prof Ediriweera Sarachchandra, that utilises the traditions of nadagama 
-                (a type of rural folk theatre in Sri Lanka). 
-            </p>
-            <a href="cwViewOwnArticle">READ MORE</a>
+                (a type of rural folk theatre in Sri Lanka). </p>
+                <a href="#">READ MORE</a>
+            </div>
         </div>
 
         <div class="card">
-            <div class="card-image image-5" ></div>
-            <h2>Maname</h2>
-            <p>A drama by the celebrated dramatist, 
-                Prof Ediriweera Sarachchandra, that utilises the traditions of nadagama 
-                (a type of rural folk theatre in Sri Lanka). 
-            </p>
-            <a href="cwViewOwnArticle">READ MORE</a>
+            <div class="imgBX">
+                <img src="<?=ROOT ?>/assets/images/sinhabahu.jpeg" alt="">
+            </div>
+            <div class="Content">
+            <h2>SINHABAHU</h2>
+                <p>Category:Tragedy</p>
+                <p>Set in the fictional Arabian city of Agrabah, the story follows the familiar tale of a poor young man who is granted three wishes by a genie in a lamp, which he uses to woo a princess and to thwart the sultan's evil Grand Vizier.</p>
+                <a href="#">READ MORE</a>
+            </div>
         </div>
 
         <div class="card">
-            <div class="card-image image-6" ></div>
-            <h2>Maname</h2>
-            <p>A drama by the celebrated dramatist, 
-                Prof Ediriweera Sarachchandra, that utilises the traditions of nadagama 
-                (a type of rural folk theatre in Sri Lanka). 
-            </p>
-            <a href="cwViewOwnArticle">READ MORE</a>
-        </div>
-
-        <!-- <div class="card">
-            <div class="card-image image-6" ></div>
-            <h2>Maname</h2>
-            <p>A drama by the celebrated dramatist, 
-                Prof Ediriweera Sarachchandra, that utilises the traditions of nadagama 
-                (a type of rural folk theatre in Sri Lanka). 
-            </p>
-            <a href="cwViewOwnArticle.view.php">READ MORE</a>
+            <div class="imgBX">
+                <img src="<?=ROOT ?>/assets/images/kuweni.jpeg" alt="">
+            </div>
+            <div class="Content">
+            <h2>KUWENI</h2>
+                <p>Category:Tragedy</p>
+                <p>Set in the fictional Arabian city of Agrabah, the story follows the familiar tale of a poor young man who is granted three wishes by a genie in a lamp, which he uses to woo a princess and to thwart the sultan's evil Grand Vizier.</p>
+                <a href="#">READ MORE</a>
+            </div>
         </div>
 
         <div class="card">
-            <div class="card-image image-6" ></div>
-            <h2>Maname</h2>
-            <p>A drama by the celebrated dramatist, 
-                Prof Ediriweera Sarachchandra, that utilises the traditions of nadagama 
-                (a type of rural folk theatre in Sri Lanka). 
-            </p>
-            <a href="cwViewOwnArticle.view.php">READ MORE</a>
-        </div> -->
+            <div class="imgBX">
+                <img src="<?=ROOT ?>/assets/images/sokari.jpeg" alt="">
+            </div>
+            <div class="Content">
+            <h2>SOKARI</h2>
+                <p>Category:Tragedy</p>
+                <p>Set in the fictional Arabian city of Agrabah, the story follows the familiar tale of a poor young man who is granted three wishes by a genie in a lamp, which he uses to woo a princess and to thwart the sultan's evil Grand Vizier.</p>
+                <a href="#">READ MORE</a>
+            </div>
+        </div>
 
-        
-    </section>
-    
-
+        <div class="card">
+            <div class="imgBX">
+                <img src="<?=ROOT ?>/assets/images/home/i5.jpeg" alt="">
+            </div>
+            <div class="Content">
+            <h2>HITHUMATHE ADARE</h2>
+                <p>Category:Tragedy</p>
+                <p>Set in the fictional Arabian city of Agrabah, the story follows the familiar tale of a poor young man who is granted three wishes by a genie in a lamp, which he uses to woo a princess and to thwart the sultan's evil Grand Vizier.</p>
+                <a href="#">READ MORE</a>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
