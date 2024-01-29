3babd41e119778b77949e77e80234819e0951d37
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2024-01-29 13:29:57 +0530

    side menu and delete function

--- testmvc/app/views/employees.view.php
+++ testmvc/app/views/employees.view.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=ROOT?>/assets/css/employees.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/employees.css">
     <title>employees</title>
 </head>
 
@@ -13,44 +14,49 @@
 <body>
     <div class="container">
         <div class="content">
-        <table>
-                        <tr>
-                            <th>Emp.No</th>
-                            <th>Employee Name</th>
-                            <th>E-mail</th>
-                            <th>NIC</th>
-                            <th>Date of Birth</th>
-                            <th>Address</th>
-                            <th>Contact</th>
-                            <th>Employee type</th>
-                            <!-- <td><a href="#" class="btn">View</a></td> -->
-                        </tr>
-                        <?php
-                        foreach ($data as $row) {
-                            echo '<tr>
-                                    <td>' . $row->id . '</td>
-                                    <td>' . $row->empName . '</td>
-                                    <td>' . $row->empEmail . '</td>
-                                    <td>' . $row->empNIC . '</td>
-                                    <td>' . $row->empDOB . '</td>
-                                    <td>' . $row->empAddress . '</td>
-                                    <td>' . $row->empContact . '</td>
-                                    <td>' . $row->empRoll . '</td>
-                                    <td><a href="#" class="btn">View</a></td>
-                                    <td><a href="#" class="btn-update">Update</a></td>
-                                    <td>
+            <table>
+                <tr>
+                    <th>Emp.No</th>
+                    <th>Employee Name</th>
+                    <th>E-mail</th>
+                    <th>NIC</th>
+                    <th>Date of Birth</th>
+                    <th>Address</th>
+                    <th>Contact</th>
+                    <th>Employee type</th>
+                    <!-- <td><a href="#" class="btn">View</a></td> -->
+                </tr>
+                <?php
+                // Check if $data is not false and is an array or object
+                if ($data && (is_array($data) || is_object($data))) {
+                    foreach ($data as $row) {
+                        echo '<tr>
+                                <td>' . $row->id . '</td>
+                                <td>' . $row->empName . '</td>
+                                <td>' . $row->empEmail . '</td>
+                                <td>' . $row->empNIC . '</td>
+                                <td>' . $row->empDOB . '</td>
+                                <td>' . $row->empAddress . '</td>
+                                <td>' . $row->empContact . '</td>
+                                <td>' . $row->empRoll . '</td>
+                                <td><a href="#" class="btn">View</a></td>
+                                <td><a href="#" class="btn-update">Update</a></td>
+                                <td>
                                     <form method="POST">
-                                    <input type="hidden" name="employee_id" value="' . $row->id . '">
-                                    <button type="submit" name="Delete" class="btn-delete">Delete</button>
+                                        <input type="hidden" name="employee_id" value="' . $row->id . '">
+                                        <button type="submit" name="Delete" class="btn-delete">Delete</button>
                                     </form>
-                            </td>
-                                  </tr>';
-                        }
+                                </td>
+                              </tr>';
+                    }
+                } else {
+                    echo '<tr><td colspan="9">No data available</td></tr>';
+                }
+                ?>
 
-                        ?>
-
-                    </table>
+            </table>
         </div>
     </div>
 </body>
+
 </html>
