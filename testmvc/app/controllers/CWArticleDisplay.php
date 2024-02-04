af14a533c87621dbf7ff81c098479e98d5e74060
Nethmi Hasinthara <www.nethmihasi2001@gmail.com>
2024-02-04 07:15:36 +0700

    responsive

--- testmvc/app/controllers/CWArticleDisplay.php
+++ testmvc/app/controllers/CWArticleDisplay.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
 /**
- * home class
+ * Display article class
  */
 class CWArticleDisplay
 {
-    use Controller;
-    public function index()
+	use Controller;
+	public function index()
 	{
 
 		// $data['username'] = empty($_SESSION['USER']) ? 'User':$_SESSION['USER']->email;
@@ -16,17 +16,18 @@ class CWArticleDisplay
 		$result = $article->findAll();
 		$data = $result;
 
-		$this->view('cwArticleDisplay',$data);
+		$this->view('cwArticleDisplay', $data);
 
-		if(isset($_POST['id'])){
+		if (isset($_POST['id'])) {
 			$articleId = $_POST['id'];
-			$this->articleDelete($articleId,$article);
+			$this->articleDelete($articleId, $article);
 		}
 		// $show($_POST);
 	}
 
-	private function articleDelete($data,$article){
-		$article ->delete($data, 'id');
+	private function articleDelete($data, $article)
+	{
+		$article->delete($data, 'id');
 		redirect("cwArticleDisplay");
 	}
 }
