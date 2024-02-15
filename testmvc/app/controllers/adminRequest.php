d136449ad542ff99ff79bdc1bf6ad5eeb10dc04f
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2024-02-15 10:24:41 +0530

    employee profile

---
+++ testmvc/app/controllers/adminRequest.php
@@ -0,0 +1,26 @@
+<?php 
+
+/**
+ * home class
+ */
+class adminRequest
+{
+	use Controller;
+
+	public function index()
+	{
+        if (empty($_SESSION['USER'])) {
+			// Redirect or handle the case when the user is not logged in
+			// For example, you might want to redirect them to the login page
+			redirect('login');
+			exit();
+		}
+
+        
+
+		// $data['username'] = empty($_SESSION['USER']) ? 'User':$_SESSION['USER']->email;
+
+		$this->view('admin/adminrequest');
+	}
+
+}
