dc371e74e4df37aade3338a0cebbc2b81d5abd50
dilki01 <dillenora@gmail.com>
2024-04-15 00:30:42 +0530

    reservarating controller added

---
+++ testmvc/app/controllers/ReservaRating.php
@@ -0,0 +1,27 @@
+<?php 
+
+// session_start();
+class ReservaRating
+{
+	use Controller;
+
+	public function index()
+	{
+		if(isset($_SESSION['USER'])){
+			show($_SESSION['USER']->username);
+		}
+		// show($_SESSION['USER']);
+		// if (isset($_SESSION['USER'])) {
+		// 	$loggedInUsername = $_SESSION['reservationists']->username;
+		
+		// 	echo "Logged-in User: $loggedInUsername";
+		// } else {
+		// 	echo "User is not logged in.";
+		// }
+
+		// show($_SESSION['USER']);
+		$data = [];
+		$this->view('ReservaRating',$data);
+	}
+
+}
