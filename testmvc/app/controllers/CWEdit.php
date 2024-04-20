20d6ac9cc47557c7f988f8d43cd6f340c4950f35
Nethmi Hasinthara <www.nethmihasi2001@gmail.com>
2024-04-20 16:23:10 +0700

    imge upload

--- testmvc/app/controllers/CWEdit.php
+++ testmvc/app/controllers/CWEdit.php
@@ -14,7 +14,59 @@ class CWEdit
         }
 
 
-        // $data['username'] = empty($_SESSION['USER']) ? 'User':$_SESSION['USER']->email;
+        // $username = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->username;
+        $employeeid = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->id;
+
+        $profiles = new Profiles;
+
+        if (isset($_POST['submit'])) {
+            // $_POST['name'] =  $username;
+
+            if ($_FILES["image"]["error"] == 4) {
+                echo
+                "<script> alert('Image Does Not Exist'); </script>";
+            } else {
+                $fileName = $_FILES["image"]["name"];
+                // $fileSize = $_FILES["image"]["size"];
+                $tmpName = $_FILES["image"]["tmp_name"];
+
+                $validImageExtension = ['jpg', 'jpeg', 'png'];
+                $imageExtension = explode('.', $fileName);
+                $imageExtension = strtolower(end($imageExtension));
+
+                if (!in_array($imageExtension, $validImageExtension)) {
+                    echo
+                    "
+                    <script>
+                      alert('Invalid Image Extension');
+                    </script>
+                    ";
+                } else {
+                    // $newImageName = uniqid();
+                    $fileNameNew = "profile" . $employeeid . '.' . $imageExtension;
+
+                    $fileDestination = $_SERVER['DOCUMENT_ROOT'] . '/Group-32/testmvc/public/assets/images/Upload/' . $fileNameNew;
+
+                    move_uploaded_file($tmpName, $fileDestination);
+                    echo
+                    "
+                    <script>
+                      alert('Successfully Added');
+                      
+                    </script>
+                    ";
+                }
+            }
+
+            $insertData = [
+                'userid' => $employeeid,
+                'name' => $_POST['name'],
+                'images' => $fileNameNew
+            ];
+            show($insertData);
+
+            $profiles->insert($insertData);
+        }
 
         $this->view('contentwriter/cwEdit');
     }
