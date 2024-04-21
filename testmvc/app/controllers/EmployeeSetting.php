6e9e4e874c7e9194f4eb96572fdfdb233654c0e5
Nethmi Hasinthara <www.nethmihasi2001@gmail.com>
2024-04-22 01:00:13 +0700

    setting

--- testmvc/app/controllers/EmployeeSetting.php
+++ testmvc/app/controllers/EmployeeSetting.php
@@ -12,16 +12,78 @@ class EmployeeSetting
 		if (empty($_SESSION['USER'])) {
 			// Redirect or handle the case when the user is not logged in
 			// For example, you might want to redirect them to the login page
-			redirect('cwLogin');
+			redirect('empLogin');
 			exit();
 		}
 
-		// $data['username'] = empty($_SESSION['USER']) ? 'User':$_SESSION['USER']->email;
+		$empid = empty($_SESSION['USER']) ? 'User':$_SESSION['USER']->id;
+		$empname = empty($_SESSION['USER']) ? 'User':$_SESSION['USER']->empName;
 
-		$emp = new Content_writers;
-		$result = $emp->findAll();
+		$emp = new Employee;
+		$profiles = new Profiles;
+		
+		if($empid){
+			$arr1['id'] = $empid;
+			$empData = $emp ->where($arr1);
 
-		$data['content_writer'] = $result;
+			if($empData){
+				$result=$empData;
+			}
+		}
+		if (isset($_POST['submit'])) {
+            // $_POST['name'] =  $username;
+
+            if ($_FILES["image"]["error"] == 4) {
+                echo
+                "<script> alert('Image Does Not Exist'); </script>";
+            } else {
+                $fileName = $_FILES["image"]["name"];
+                // $fileSize = $_FILES["image"]["size"];
+                $tmpName = $_FILES["image"]["tmp_name"];
+
+                $validImageExtension = ['jpg', 'jpeg', 'png'];
+                $imageExtension = explode('.', $fileName);
+                $imageExtension = strtolower(end($imageExtension));
+
+                if (!in_array($imageExtension, $validImageExtension)) {
+                    echo
+                    "
+                    <script>
+                      alert('Invalid Image Extension');
+                    </script>
+                    ";
+                } else {
+                    // $newImageName = uniqid();
+                    $fileNameNew = "profile" . $empid . '.' . $imageExtension;
+
+                    $fileDestination = $_SERVER['DOCUMENT_ROOT'] . '/Group-32/testmvc/public/assets/images/Upload/' . $fileNameNew;
+
+                    move_uploaded_file($tmpName, $fileDestination);
+                    echo
+                    "
+                    <script>
+                      alert('Successfully Added');
+                      
+                    </script>
+                    ";
+                }
+            }
+
+            $insertData = [
+                'userid' => $empid,
+                'name' => $empname,
+                'images' => $fileNameNew
+            ];
+            // show($insertData);
+
+            $profiles->insert($insertData);
+        }
+
+
+
+
+		$data['emp'] = $result;
+        
 
 		$this->view('employee/employeeSetting', $data);
 	}
