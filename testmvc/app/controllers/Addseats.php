600f2f9b928dd89a1066e6d9585aa7b624dfe473
I.C Senanayake <ishanchami9@gmail.com>
2024-04-24 22:48:23 +0530

    update addseats validation

--- testmvc/app/controllers/Addseats.php
+++ testmvc/app/controllers/Addseats.php
@@ -11,33 +11,99 @@ class Addseats
     {
         $data = [];
         
-		
+        //_________________________Check validation__________________________________
 		if($_SERVER['REQUEST_METHOD'] == "POST")
 		{
-
-			$home = new Homes;
-
-            $data['title'] = $_POST['title'];
-            $data['description'] = $_POST['description'];
-            $data['image'] = $_POST['image'];
-
-
-            if($home->validate($data))
+            if(isset($_POST['title']) && $_POST['title']==null)
             {
-                //_______add drama into home table_________
-                $arr['title'] = $_POST['title'];
-                $arr['description'] = $_POST['description'];
-                $arr['image'] = $_POST['image'];
-				$home->insert($arr);
-
+                $data['no_title'] = 'Drama title is required';
             }
-            else
+            if(isset($_POST['description']) && $_POST['description']==null)
             {
-                $data['errors'] = $home->errors;   
+                $data['no_description'] = 'Drama description is required';
+            }
+            if ($_FILES["image"]["error"] == 4) 
+            {
+                $data['no_image']= 'Image Does Not Exist';
             }
 
-		}
 
-		$this->view('/ticket_booking/addseats', $data);
+
+
+        //________________Check and insert data into database_____________________________________
+            if(isset($_POST['title'])  &&  isset($_POST['description']))
+            {
+                if($_POST['title']!=null && $_POST['description']!=null)
+                {
+
+                    $home = new Homes;
+
+                    $data['title'] = $_POST['title'];
+                    $data['description'] = $_POST['description'];
+                    $data['image'] = $_FILES['image'];
+
+                    if ($_FILES["image"]["error"] == 4) 
+                    {
+                        $data['no_image_v']= 'Image Does Not Exist';
+                    } 
+                    else 
+                    {
+                        $fileName = $_FILES["image"]["name"];
+                        $tmpName = $_FILES["image"]["tmp_name"];
+
+                        $imageExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+                        $imageExtension_lc = strtolower($imageExtension);
+                        $validImageExtension = ["jpg", "jpeg", "png", "webp"];
+
+                        if (!in_array($imageExtension_lc, $validImageExtension)) 
+                        {
+                            $data['extns_invalid'] = 'Invalid Image Extension';
+                        } 
+                        else 
+                        {
+                            $homesss = new Homes;
+                            $row = $homesss->findAll();
+
+                            if (is_array($row) && !empty($row)) 
+                            {
+                                $last_element = end($row);      
+                                $img_name = $last_element->id + 1;
+                            } 
+                            else 
+                            {
+                                $img_name = 1;
+                            }
+
+                            $fileNameNew = $img_name.'.'.$imageExtension;
+                        //show($fileNameNew);
+
+                            $fileDestination = $_SERVER['DOCUMENT_ROOT'] . '/Group-32/testmvc/public/assets/images/drama_img/' . $fileNameNew;
+
+                            move_uploaded_file($tmpName, $fileDestination);
+
+
+                        // $data['title'] = $_POST['title'];
+                            //$data['description'] = $_POST['description'];
+                            //$data['image'] = $fileNameNew;
+
+                            
+                                //_______add drama into home table_________
+                                $arr['title'] = $_POST['title'];
+                                $arr['description'] = $_POST['description'];
+                                $arr['image'] = $data['image'];
+                                $home->insert($arr);
+
+                                redirect('addtimes');
+                        }
+
+                    }
+                }
+                else
+                {
+                    $data['not_all'] = 'Please fill all the fields';
+                }
+            }
+        }
+        $this->view('/ticket_booking/addseats', $data);
     }
 }
