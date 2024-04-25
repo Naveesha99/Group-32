38e7161045e131e1d8189bb5c890ca1615f11f25
dilki01 <dillenora@gmail.com>
2024-04-26 00:31:50 +0530

    dilki

--- testmvc/app/models/Reservationpayments.php
+++ testmvc/app/models/Reservationpayments.php
@@ -25,27 +25,24 @@ class Reservationpayments
 	{
 		$this->errors = [];
 
-		// if (empty($data['reqid'])) {
-		// 	$this->errors['reqid'] = "reqid is required";
-		// }
 
-		// if (empty($data['fullname'])) {
-		// 	$this->errors['fullname'] = "Full name is required";
-		// }
+		if (empty($data['fullname'])) {
+			$this->errors['fullname'] = "Full name is required";
+		}
 
-		// if (empty($data['mobile'])) {
-		// 	$this->errors['mobile'] = "mobile is required";
-		// } else
-		// if (!filter_var($data['mobile'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[0-9]{10}$/")))) {
-		// 	$this->errors['mobile'] = "mobile is not valid";
-		// }
+		if (empty($data['mobile'])) {
+			$this->errors['mobile'] = "mobile is required";
+		} else
+		if (!filter_var($data['mobile'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[0-9]{10}$/")))) {
+			$this->errors['mobile'] = "mobile is not valid";
+		}
 		
-		// if (empty($data['email'])) {
-		// 	$this->errors['email'] = "Email is required";
-		// } else
-		// if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-		// 	$this->errors['email'] = "Email is not valid";
-		// }
+		if (empty($data['email'])) {
+			$this->errors['email'] = "Email is required";
+		} else
+		if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+			$this->errors['email'] = "Email is not valid";
+		}
 
 		// if (empty($data['ispaid'])) {
 		// 	$this->errors['ispaid'] = "ispaid is required";
