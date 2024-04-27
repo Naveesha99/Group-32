11a034214875eb22155983f989218d0261bad489
I.C Senanayake <ishanchami9@gmail.com>
2024-04-27 20:20:15 +0530

    update Home controller

--- testmvc/app/controllers/Home.php
+++ testmvc/app/controllers/Home.php
@@ -9,36 +9,35 @@ class Home
 
 	public function index()
 	{
-
+		$data = [];
 		$booking = new Booking;
 
 		$row= $this->get_drama_img();
 
-		$i=0;
-		foreach($row as $key)
-		{			
-			
-			$arr['drama_id'] = $key->id;
-			
-
-			// show($arr['id']);
-			$row2 = $booking->where($arr);
-			// show($row2);
-			if ($row2 !== false && is_array($row2))
-			{
-
-				foreach($row2 as $k)
-				{
-					$data['data2'][$i] = $k;
-					$i=$i+1;
+		if($row)
+		{
+				$i=0;
+				foreach($row as $key)
+				{			
+					$arr['drama_id'] = $key->id;
+					
+					// show($arr['id']);
+					$row2 = $booking->where($arr);
+					// show($row2);
+					if ($row2 !== false && is_array($row2))
+					{
+						foreach($row2 as $k)
+						{
+							$data['data2'][$i] = $k;
+							$i=$i+1;
+						}
+					}
+									
 				}
-			}
-							
+
+				$data['data1']= $row;
 		}
-
-
-		$data['data1']= $row;
-       
+		
         $this->view('home', $data);
 	}
 
