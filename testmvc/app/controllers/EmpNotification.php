0b9655175182737ac9c87e981fff806779f1fe3f
Nethmi Hasinthara <www.nethmihasi2001@gmail.com>
2024-04-29 04:03:33 +0700

    notifications

---
+++ testmvc/app/controllers/EmpNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+class EmpNotification
+{
+    use Controller;
+
+    public function index()
+    {
+        $notification = new Notification();
+        $arr['target_userID'] = $_SESSION['USER']->id;
+        $result = $notification->where($arr);
+        $data['notifications'] = $result;
+        // show($result);
+        
+        // if($result){
+        //     foreach($result as $row){
+        //         $message[] = $row;
+        //     }
+        //     show($message);
+        // }
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $id = $_POST['id'];
+            $sendData['isRead'] = 1;
+            if($notification->update($id, $sendData)){
+                redirect('notification');
+            }
+        }
+
+
+        $this->view('employee/empNotification', $data);
+
+    }
+}
