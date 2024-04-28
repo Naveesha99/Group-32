f93a35ef6d65c3b3785364ba4110747431421424
Nethmi Hasinthara <www.nethmihasi2001@gmail.com>
2024-04-29 04:05:56 +0700

    history

--- testmvc/app/views/employee/empHistory.view.php
+++ testmvc/app/views/employee/empHistory.view.php
@@ -12,6 +12,7 @@
 <?php include 'navBar.php' ?>
 
 <body>
+    <div id="overlay"></div>
     <div class="container">
         <form>
             <div class="form">
@@ -63,29 +64,63 @@
                                 <th>Option</th>
                             </tr>
                         </thead>
-                        
+
                         <tbody>
                             <?php
                             if ($historyTasks) {
                                 foreach ($historyTasks as $row) {
-                                    echo '<tr>
-                            <td>' . $row->taskType . ' </td>
-                            <td>' . $row->location . ' </td>
-                            <td>' . $row->date . ' </td>
-                            <td>' . $row->startTime . ' </td>
-                            <td>' . $row->endTime . ' </td>
-                            <td>' . $row->status . '</td>
-                            <td> 
-                            <span class="action_btn">
-                                    <a href = "empTaskView?id=' . $row->id . '" class = "btn">View</a>
-                                </span>
-                                </td>
-                            </tr>';
+                            ?>
+                                    <tr>
+                                        <td><?= $row->taskType ?> </td>
+                                        <td><?= $row->location ?> </td>
+                                        <td><?= $row->date ?> </td>
+                                        <td><?= $row->startTime ?> </td>
+                                        <td><?= $row->endTime ?> </td>
+                                        <td><?= $row->status ?></td>
+                                        <td>
+                                            <span class="action_btn">
+                                                <form method="POST">
+                                                    <input type="hidden" name="task_id" value="' . $row->id . '">
+                                                    <button type="button" data-order='<?= json_encode($row) ?>' class="btn" onClick="openPopup(this)">View</button>
+                                                </form>
+                                            </span>
+                                        </td>
+                                    </tr>
+                            <?php
                                 }
                             } else {
                                 echo '<tr><td colspan="6">No data available</td></tr>';
                             }
                             ?>
+
+                            <div class="card1" id="card1">
+                                <div class="box">
+                                    <div class="content">
+
+                                        <div class="paragraph">
+                                            <label for="taskType">Task:</label>
+                                            <input type="text" id="taskType" name="taskType" readonly>
+
+                                            <label for="location">Place:</label>
+                                            <input type="text" id="location" name="location" readonly>
+
+                                            <label for="date">Date:</label>
+                                            <input type="text" id="date" name="date" readonly>
+
+                                            <label for="startTime">Start Time:</label>
+                                            <input type="text" id="startTime" name="startTime" readonly>
+
+                                            <label for="endTime">End Time:</label>
+                                            <input type="text" id="endTime" name="endTime" readonly>
+
+                                            <label for="status">Status:</label>
+                                            <input type="text" id="status" name="status" readonly>
+                                            <button onclick="closePopup()">Close</button>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
 
                         </tbody>
 
@@ -120,6 +155,26 @@
                 row.classList.toggle('hide', row_text.indexOf(search_data) < 0);
             })
         }
+
+        function openPopup(button) {
+            var order = JSON.parse(button.getAttribute("data-order"));
+            document.getElementById('taskType').value = order.taskType;
+            document.getElementById('location').value = order.location;
+            document.getElementById('date').value = order.date;
+            document.getElementById('startTime').value = order.startTime;
+            document.getElementById('endTime').value = order.endTime;
+            document.getElementById('status').value = order.status;
+            document.getElementById('card1').style.display = 'block';
+            document.getElementById('overlay').style.display = 'block';
+            
+        };
+
+        function closePopup() {
+            document.getElementById('card1').style.display = 'none';
+            document.getElementById('overlay').style.display = 'none';
+            window.location.href = "EmpHistory";
+
+        };
     </script>
 
 
