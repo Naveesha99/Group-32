286caf435495650c08ffcd1f43fcc162bbafd9a7
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2024-04-29 20:25:50 +0530

    add drama

--- testmvc/app/controllers/addDrama.php
+++ testmvc/app/controllers/addDrama.php
@@ -10,41 +10,121 @@ class addDrama
     public function index()
     {
         $data = [];
+        
+        //Check validation_________________________________
+		if($_SERVER['REQUEST_METHOD'] == "POST")
+		{
+            if(isset($_POST['title']) && $_POST['title']==null)
+            {
+                $data['no_title'] = 'Drama title is required';
+            }
+            if(isset($_POST['description']) && $_POST['description']==null)
+            {
+                $data['no_description'] = 'Drama description is required';
+            }
+            if ($_FILES["image"]["error"] == 4)
+            {
+                $data['no_image']= 'Image Does Not Exist';
+            }
 
 
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
-            $home = new Homes;
-
-            $data['title'] = $_POST['title'];
-            $data['description'] = $_POST['description'];
-            $data['image'] = $_POST['image'];
 
 
-            if ($home->validate($_POST)) {
+        //_Check and insert data into database____________________________________
+            if(isset($_POST['title'])  &&  isset($_POST['description']))
+            {
+                if($_POST['title']!=null && $_POST['description']!=null)
+                {
+                    $abc['title'] = $_POST['title'];
 
+                    $home = new Homes;
 
-                //_______add drama into home table_________
-                $arr['title'] = $_POST['title'];
-                $arr['description'] = $_POST['description'];
-                $arr['image'] = $_POST['image'];
-                $arr1['title'] = $_POST['title'];
-                $result = $home->first($arr1);
+                    $data['title'] = $_POST['title'];
+                    $data['description'] = $_POST['description'];
+                    $data['image'] = $_FILES['image'];
 
-                if ($result) {
-                    $data['errors']['added'] = "This drama is already added";
+                    $res = $home->first($abc);
 
-                } else {
-                    $home->insert($arr);
+                if($res)
+                {
+                    $data['exist_title'] = "Already exist this drama";
                 }
+                else
+                {
+                    if(!empty($data['description']) && strlen($data['description']) < 100)
+                    {
+                        $data['des_length'] = "Description must be at least 100 characters";
+                    }
+                    else
+                    {
+                            
+                        if ($_FILES["image"]["error"] == 4) 
+                        {
+                            $data['no_image_v']= 'Image Does Not Exist';
+                        } 
+                        else 
+                        {
+                            $fileName = $_FILES["image"]["name"];
+                            $tmpName = $_FILES["image"]["tmp_name"];
 
-                // $home->insert($arr);
+                            $imageExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+                            $imageExtension_lc = strtolower($imageExtension);
+                            $validImageExtension = ["jpg", "jpeg", "png", "webp"];
 
-            } else {
-                $data['errors'] = $home->errors;
+                            if (!in_array($imageExtension_lc, $validImageExtension)) 
+                            {
+                                $data['extns_invalid'] = 'Invalid Image Extension';
+                            } 
+                            else 
+                            {
+                                $homesss = new Homes;
+                                $row = $homesss->findAll();
+
+                                if (is_array($row) && !empty($row)) 
+                                {
+                                    $last_element = end($row);      
+                                    $img_name = $last_element->id + 1;
+                                } 
+                                else 
+                                {
+                                    $img_name = 1;
+                                }
+
+                                $fileNameNew = $img_name.'.'.$imageExtension;
+                            //show($fileNameNew);
+
+                                $fileDestination = $_SERVER['DOCUMENT_ROOT'] . '/Group-32/testmvc/public/assets/images/drama_img/' . $fileNameNew;
+
+                                move_uploaded_file($tmpName, $fileDestination);
+
+
+                            // $data['title'] = $_POST['title'];
+                                //$data['description'] = $_POST['description'];
+                                // $data['image'] = $fileNameNew;
+
+                                
+                                    //add drama into home table________
+                                    $arr['title'] = $_POST['title'];
+                                    $arr['description'] = $_POST['description'];
+                                    $arr['image'] = $fileNameNew;
+                                    $home->insert($arr);
+
+                                    $data['ok'] = 'Drama Added successed...!';
+                                    redirect('addtimes');
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+            else
+            {
+                $data['not_all'] = 'Please fill all the fields';
+            }
             }
         }
-
-        $this->view('admin/adddrama', $data);
+        $this->view('/admin/adddrama', $data);
     }
 }
--- testmvc/app/views/admin/addDrama.view.php
+++ testmvc/app/views/admin/addDrama.view.php
@@ -15,41 +15,33 @@
 <body>
 	<div class="container">
 		<div class="content">
-			<form method="POST" class="add-Drama">
+			<form  method="POST"  enctype="multipart/form-data" >
 				<h1>ADD DRAMA</h1>
 				<label for="title">Drama Name</label>
-				<?php if (!empty($errors['title'])) : ?>
-                <span class="error">
-                    <?= '* ' . $errors['title'] ?> </br>
-                </span>
+				<input type="text" name="title" placeholder="title">
 				
-            <?php endif; ?>
-			<?php if (!empty($errors['added'])) : ?>
-                <span class="error">
-                    <?= '* ' . $errors['added'] ?> </br>
-                </span>
-				
-            <?php endif; ?>
-				<input type="text" name="title">
+			<?php if(isset($data['no_title'])){?>  <span class="error"><?= '* ' .$data['no_title'] ?></span> <?php } ?>
+			<?php if(isset($data['exist_title'])){?>  <span class="error"><?= '* ' .$data['exist_title'] ?></span> <?php } ?>
+
                 
 				<label for="description">Description</label>
-				<?php if (!empty($errors['description'])) : ?>
-                <span class="error">
-                    <?= '* ' . $errors['description'] ?> </br>
-                </span>
-            <?php endif; ?>
+				<?php if(isset($data['no_description'])){?>  <span class="error"><?= '* ' .$data['no_description'] ?></span> <?php } ?>
+				<?php if(isset($data['des_length'])){?>  <span class="error"><?= '* ' .$data['des_length'] ?></span> <?php } ?>
 				<input type="text" name="description">
 
 				<label for="image">Image</label>
-				<?php if (!empty($errors['image'])) : ?>
-                <span class="error">
-                    <?= '* ' . $errors['image'] ?> </br>
-                </span>
-            <?php endif; ?>
-				<input type="file" name="image">
+				<?php if(isset($data['no_image'])){?>  <span class="error"><?= '* ' .$data['no_image'] ?></span> <?php } ?>
+						<?php if(isset($data['not_all'])){?>  <span class="error"><?= '* ' .$data['not_all'] ?></span> <?php } ?>
+				<input type="file"  id="image" name="image" accept="image/*">
 				
+				<?php if(!empty($errors)):?>
+							<h5>
+								<?= implode("<br>",$errors)?>
+							</h5>
+				<?php endif;?>
 
 				<button class="btn-1">Add Drama</button>
+				<?php if(isset($data['ok'])){?>  <span class="error"><?= '* ' .$data['ok'] ?></span> <?php } ?>
 			</form>
 		</div>
 	</div>
