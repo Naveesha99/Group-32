4ec8cc5bd44399ce0174ee2fdb3358c17fe8d26f
dilki01 <dillenora@gmail.com>
2024-04-29 21:49:50 +0530

    dilki

--- testmvc/app/views/reservaQRscanner.view.php
+++ testmvc/app/views/reservaQRscanner.view.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QR Scanner</title>
-    <link rel="stylesheet" href="<?=ROOT?>/assets/css/ticket_booking/qrscanner.css">
+    <!-- <link rel="stylesheet" href="<?=ROOT?>/assets/css/ticket_booking/qrscanner.css"> -->
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/ticket_booking/check_viewers.css">
+
 </head>
 
 <body>
     <div class="container">
-        <h3>SCAN THE QR</h3>
+        <div class="container1">
+        <h3>Find Booking and Refund Details</h3>
         <div style="display:flex; justify-content: center;">
             <div id="my-qr-reader"style="width:500px;"></div>
         </div>
@@ -19,12 +22,9 @@
         </form>
     </div>
 
-    <div class="container">
-        <!-- <?//php show($data); ?> -->
-    </div>
 
 
-    <div class="container">
+    <div class="container2">
 
     <?php
     
@@ -32,7 +32,9 @@
             if(isset($data['row']))
             {
                $x = $data['row'];
-        ?>
+            ?>
+            <div class="indetails">
+
                 <div class="order_data">Order ID : <?= $x[0]->id ?></div>
                 <div class="order_data">Hall Name : <?= $x[0]->hallno ?></div>
                 <div class="order_data">Date : <?= $x[0]->date ?></div>
@@ -43,14 +45,50 @@
                 <div class="order_data">standings : <?= $x[0]->standings ?></div>
                 <div class="order_data">amount : <?= $x[0]->amount ?></div>
                 <div class="order_data">status : <?= $x[0]->status ?></div>
+                <div class="order_data"> Reservationist ID : <?= $x[0]->reservationistId ?></div>
+                <div class="order_data">Is Paid : <?= $x[0]->ispaid ?></div>
+                <div class="order_data">has Arrived : 
+                    <?php 
+                        if($x[0]->hasArrived == 1){
+                            echo "Yes";
+                        }else{
+                            echo "No";
+                        }; ?> 
+                </div>                                              
+                <div class="order_data">refundAmount : <?= $x[0]->refundedAmount ?></div>
 
 
-        <?php
-            }
-        ?>
+
+                <?php if(isset($x[0]->refundedAmount))
+                    {
+                        if($x[0]->refundedAmount > 0)
+                        { ?>
+                            <div class="order_data">
+                                <p>Refund Details : <?= $x[0]->refundedAmount; ?></div></p>
+                            </div>
+                            <form method="post">
+                                <input type="text" value="<?= $x[0]->id ?>" name="reqid">
+                                <input type="text" value="refunded" name="status">
+                                <button type="submit">Confirm Refund</button>
+                            </form>
+                        <?php 
+                        }
+                        else { ?>
+                            <div class="order_data">
+                            <p>Has arrived</p>
+                            </div>
+                            <form action="" method="post">
+                                <input type="text" value="<?= $x[0]->id ?>" name="reqid">
+                                <input type="text" value="1" name="isArrived">
+                                <button type="submit">Confirm Arrival</button>
+                            </form>
+
+                   <?php }
+                    }
+                ?>
     </div>
 
-
+<?php }?>
 
     <!-- LOAD Library__ -->
     <script src="https://unpkg.com/html5-qrcode"></script>
@@ -92,4 +130,6 @@
                 htmlscanner.render(onScanSuccess)
         })
     </script>
+
+    </div>
 </body>
